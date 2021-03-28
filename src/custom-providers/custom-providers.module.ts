78882ff633cb9d2b99b77e8ab36937dbcc1a76e7
rosy <rosyshrestha028@gmail.com>
2021-03-28 19:56:57 +0545

    [CUSTOM_PROVIDER] : USe non-class based provider tokens

--- src/custom-providers/custom-providers.module.ts
+++ src/custom-providers/custom-providers.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { COFFEE_BRANDS } from 'src/custom-providers/coffees.constants';
 import { CustomProvidersService } from './custom-providers.service';
 
 export class MockCoffeeService {}
 @Module({
   providers: [
     { provide: CustomProvidersService, useValue: new MockCoffeeService() },
+    { provide: COFFEE_BRANDS, useValue: ['srew', 'mescafe'] },
   ],
 })
 export class CustomProvidersModule {}
