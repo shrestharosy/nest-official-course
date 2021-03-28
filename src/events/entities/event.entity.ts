44d56d7e307756e1b1aa1493589ce7d2bc7d4ca2
rosy <rosyshrestha028@gmail.com>
2021-03-28 17:33:17 +0545

    [INDEX] : Add indexes to event entities

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,5 +1,13 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+// ===================================NOTES ==========================================
+// Indexes are special lookup tables that a db search engine can use to retrieve data fast
+
+// advanced use case
+// composite index i.e contains multiple columns
+// apply index decorator to the class itself and pass array of column names
+
+// @Index(['name', 'type'])
 export class Event {
   @PrimaryGeneratedColumn()
   id: string;
@@ -7,6 +15,9 @@ export class Event {
   @Column()
   type: string;
 
+  // say common operation is retrieving event by name
+  //  so add Index decoartor to the name column
+  @Index()
   @Column()
   name: string;
 
