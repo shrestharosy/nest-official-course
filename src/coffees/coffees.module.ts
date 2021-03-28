a88fb99bbade73f454d0f15463b91a6ab36d5596
rosy <rosyshrestha028@gmail.com>
2021-03-28 12:04:54 +0545

    [ENTITY] : Create and register customer TypeORM entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Coffee } from './entities/coffee.entity';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
-import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])], //pass array of entities here
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity() //sql table === 'coffee'
+// pass name in decorato if you want a different name @Entity('coffees')
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
