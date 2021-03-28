4467ca2e1d1cad6bc4a3c18fd5093f0bb49cda83
rosy <rosyshrestha028@gmail.com>
2021-03-28 14:47:43 +0545

    [CONFIG] : Add env keys to postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,13 +7,14 @@ import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5433, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
+      host: process.env.DB_HOST, // database host
+      port: 5432, // database host
+      username: process.env.DB_USERNAME, // username
+      password: process.env.DB_PASSWORD, // user password
       database: 'postgres', // name of our database,
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
