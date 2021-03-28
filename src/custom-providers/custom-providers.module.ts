794cff8ba039c4a820a799886305d7d0bafa6a43
rosy <rosyshrestha028@gmail.com>
2021-03-28 20:03:14 +0545

    [CUSTOM_PROVIDERS] : Add class providers

--- src/custom-providers/custom-providers.module.ts
+++ src/custom-providers/custom-providers.module.ts
@@ -3,10 +3,21 @@ import { COFFEE_BRANDS } from 'src/custom-providers/coffees.constants';
 import { CustomProvidersService } from './custom-providers.service';
 
 export class MockCoffeeService {}
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 @Module({
   providers: [
     { provide: CustomProvidersService, useValue: new MockCoffeeService() },
     { provide: COFFEE_BRANDS, useValue: ['srew', 'mescafe'] },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
   ],
 })
 export class CustomProvidersModule {}
