63e2651762f631ac214c122ce74c1bf71895fcf9
rosy <rosyshrestha028@gmail.com>
2021-03-28 21:46:06 +0545

    [CONFIG] : Asynchronously configure dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,15 +23,19 @@ import { CustomProvidersModule } from './custom-providers/custom-providers.modul
       // load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: process.env.DB_HOST, // database host
-      port: 5432, // database host
-      username: process.env.DB_USERNAME, // username
-      password: process.env.DB_PASSWORD, // user password
-      database: 'postgres', // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+    // asynchronously load configuration options with forRootAsync; async config is loaded after every model registered in the application is resolved
+    // so order of imports in the module does not matter anymore
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.DB_HOST, // database host
+        port: 5432, // database host
+        username: process.env.DB_USERNAME, // username
+        password: process.env.DB_PASSWORD, // user password
+        database: 'postgres', // name of our database,
+        autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+        synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+      }),
     }),
     CoffeeRatingModule,
     CustomProvidersModule,
