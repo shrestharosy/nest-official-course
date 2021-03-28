d376bbee7222167efdf5e23f324106b703508d27
rosy <rosyshrestha028@gmail.com>
2021-03-28 18:47:18 +0545

    [CUSTOM-PROVIDERS] : Add value based providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,11 +6,13 @@ import { Flavor } from './entities/flavor.entity';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { RecommendationEvent } from 'src/events/entities/event.entity';
+import { CustomCoffeeService } from './customProvider/custom.coffee.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, RecommendationEvent])], //pass array of entities here
   controllers: [CoffeesController],
   providers: [CoffeesService],
+  // providers: [{ provide: CoffeesService, useValue: new CustomCoffeeService() }],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
