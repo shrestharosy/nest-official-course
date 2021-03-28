7ff6ccfd3a68c9622e2ac72bde6349a36e68d4ea
rosy <rosyshrestha028@gmail.com>
2021-03-28 18:39:24 +0545

    [ENCAPSUATION] : Use coffee service provider in coffee-rating module

---
+++ src/coffee-rating/coffee-rating.module.ts
@@ -0,0 +1,9 @@
+import { CoffeesModule } from './../coffees/coffees.module';
+import { Module } from '@nestjs/common';
+import { CoffeeRatingService } from './coffee-rating.service';
+
+@Module({
+  imports: [CoffeesModule],
+  providers: [CoffeeRatingService],
+})
+export class CoffeeRatingModule {}
